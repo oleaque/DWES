50f0e5ea9dda2cea04e558aa41e66f96d542ed30
oleaque <joolse@alumnatflorida.es>
2025-09-23 10:46:03 +0200

    act ap2_1

---
+++ bloque1/ap2_1.php
@@ -0,0 +1,66 @@
+<?php
+
+class vehiculoCarrera
+{
+    protected $marca;
+    protected $modelo;
+    protected $velocidad;
+    protected $combustible;
+
+    public function __construct($marca, $modelo, $velocidad, $combustible)
+    {
+        $this->marca = $marca;
+        $this->modelo = $modelo;
+        $this->velocidad = $velocidad;
+        $this->combustible = $combustible;
+    }
+
+    protected function consumirCombustible($combustible, $velocidad)
+    {
+        if ($this->velocidad > 1 && $this->velocidad < 10) {
+            echo $this->combustible - 0.1;
+        }
+    }
+
+    public function arrancar($velocidad)
+    {
+        if ($this->velocidad > 1) {
+            echo "el coche esta encendido";
+        }
+
+    }public function acelerar($velocidad){
+        if($this->velocidad>15){
+            echo "el coche esta acelerando";
+        }
+}public function detener($velocidad){
+        if($this->velocidad<1){
+            echo "el coche se ha detenido";
+        }
+}public function mostrarEstado($marca,$modelo,$velocidad,$combustible){
+        echo "la marca es: $this->marca";
+    echo "el modelo es: $this->modelo";
+    echo " la velocidad es: $this->velocidad";
+    echo "el combustible: $this->combustible";
+} public function __destruct()
+{
+    echo "el $this->modelo se ha retirado";
+}
+
+
+} class cocheF1 extends vehiculoCarrera{
+
+    private $alerones;
+
+    pubic function __construct($marca, $modelo, $velocidad, $combustible,$alerones)
+    {
+        parent::__construct($marca, $modelo, $velocidad, $combustible);
+        $this->alerones=$alerones;
+    }public function activarDRS()
+
+}class cocheElectricoF1{
+pubic function __construct($marca, $modelo, $velocidad, $combustible,$bateria){
+    parent::__construct($marca, $modelo, $velocidad, $combustible);
+        $this->bateria=$bateria;
+    }
+
+}
