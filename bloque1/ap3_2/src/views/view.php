8a747d524afa2b2d25260a5ed1947302a232b01e
joan <joanoleaque@gmail.com>
2025-10-06 18:33:58 +0200

    acabada

--- bloque1/ap3_2/src/views/view.php
+++ bloque1/ap3_2/src/views/view.php
@@ -7,8 +7,9 @@ class ProductoVista{
     public function setLista($lista){
         $this->lista=$lista;
     }
-    public function mostrar(){
-        $html='<!DOCTYPE html>
+    public function mostrar()
+    {
+        $html = '<!DOCTYPE html>
 <html>
 <head>
     <title>Lista de productos</title>
@@ -22,20 +23,41 @@ class ProductoVista{
             <th>Precio</th>
             <th>Descripción</th>
         </tr>';
-        foreach($this->lista as $lista){
+        foreach ($this->lista as $lista) {
             $html .= '<tr>
+            <td>' . htmlspecialchars($lista['id']) . '</td>
             <td>' . htmlspecialchars($lista['titulo']) . '</td>
-            <td>' . htmlspecialchars($lista['keywords']) . '</td>
             <td>' . number_format($lista['descripcion'], 2) . ' </td>
-            <td>' . htmlspecialchars($lista['contenido']) . '</td>
+            <td>' . htmlspecialchars($lista['fecha_creacion']) . '</td>
+            <td>' . htmlspecialchars($lista['fecha_vencimiento']) . '</td>
         </tr>';
         }
-            $html .= '
+        $html .= '
             </table>
 </body>
 </html>';
 
         echo $html;
-
     }
+    public static function MostrarL($lista) {
+            $vista = new self();
+            $vista->setLista($lista);
+            $vista->mostrar();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
