93183673a57964e257835d17d7dcd667cadbff7d
oleaque <joolse@alumnatflorida.es>
2025-11-07 09:46:00 +0100

    5-1

--- bloque2/AP5/AP5-1/src/Entity/Detail.php
+++ bloque2/AP5/AP5-1/src/Entity/Detail.php
@@ -3,6 +3,12 @@
 namespace AP51\Entity;
 
 
+use AP4_1\repository\TaskRepository;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\Table;
 
 #[Entity(repositoryClass: TaskRepository::class)]
 #[Table(name: 'DETALLE')]
@@ -14,15 +20,14 @@ class detail
     private int $product;
 
 
-    #[Column(name:'PRECIO_VENTA', type:'decimal')]
+    #[Column(name: 'PRECIO_VENTA', type: 'decimal')]
     private int $selling_price;
 
-    #[Column(name:'CANTIDAD', type:'integer')]
+    #[Column(name: 'CANTIDAD', type: 'integer')]
     private int $quantity;
 
-    #[Column(name:'importe', type:'decimal')]
+    #[Column(name: 'IMPORTE', type: 'decimal')]
     private int $import;
-
 
 
 }
--- bloque2/AP5/AP5-1/src/Entity/order.php
+++ bloque2/AP5/AP5-1/src/Entity/order.php
@@ -3,28 +3,31 @@
 namespace AP51\Entity;
 
 
-
+use AP4_1\repository\TaskRepository;
 use DateTime;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\Table;
 
 #[Entity(repositoryClass: TaskRepository::class)]
 #[Table(name: 'PEDIDO')]
+class order
+{
 
-class order{
+    #[column(name: 'PEDIDO_FECHA', type: 'date')]
+    private DateTime $date;
 
-    #[column(name:'pedido_fecha', type: 'date')]
-private DateTime $date;
+    #[column(name: 'PEDIDO_TIPO', type: 'string')]
+    private string $type;
 
-    #[column(name:'pedido_tipo', type:'string')]
-private string $type;
+    #[column(name: 'CLIENTE_COD', type: 'integer')]
+    private int $client_code;
 
-    #[column(name:'cliente_cod', type:'integer')]
-private int $client_code;
+    #[column(name: 'FECHA_ENVIO', type: 'date')]
+    private DateTime $date_shipping;
 
-    #[column(name:'fecha_envio', type:'date')]
-private DateTime $date_shipping;
-
-    #[column(name:'total', type:'decimal')]
-private int $total;
+    #[column(name: 'TOTAL', type: 'decimal')]
+    private int $total;
 
 }
 
